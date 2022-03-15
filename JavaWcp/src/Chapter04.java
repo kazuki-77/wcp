dad5a7b8eb4e956901a9527e4ad427c829ed29f5
kauzki-77 <k.sora.7861@gmail.com>
2022-03-15 17:14:56 +0900

    [finish]Chapter4

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,11 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String str = "自分の名前";
+		System.out.println(str);
+		str = "好きな言葉";
+		System.out.println(str);
+		final int NUMBER = 7;
+		System.out.println(NUMBER);
+	}
+}
---
+++ JavaWcp/src/Variable01.java
@@ -0,0 +1,20 @@
+
+public class Variable01 {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int num1; // int型の変数num1を宣言
+		String str1; // String型の変数str1を宣言
+		num1 = 10; // num1を初期化
+		str1 = "Hello World!"; //　str1を初期化
+		System.out.println(num1);
+		System.out.println(str1);
+		
+		num1 = 20;  // num1に20を再代入
+		System.out.println(num1);
+		
+		int num2 = 30; // int型のnum2を宣言し同時に初期化
+		System.out.println(num2);
+	}
+}
