1a35e3ec5bb0ae8c6b95eda5069160104c59a516
kauzki-77 <k.sora.7861@gmail.com>
2022-04-10 21:49:09 +0900

    [add]java基礎復習

---
+++ Sample/src/sample/practice.java
@@ -0,0 +1,19 @@
+package sample;
+
+public class practice {
+	public static void main(String[] args) {
+		int sum = 0;
+		int count = 0;
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i < 10; i++) {
+			if(i % 2 == 1) {
+				System.out.println(i);
+			}
+		}
+}
+
+}
