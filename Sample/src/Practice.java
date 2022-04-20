fdd287787501240b7dd4ca270f26b1b9e019b991
kauzki-77 <k.sora.7861@gmail.com>
2022-04-20 21:13:59 +0900

    [add] 1~10までの数字のうち偶数のみを昇順で表示させる

--- Sample/src/Practice.java
+++ Sample/src/Practice.java
@@ -1,23 +1,10 @@
 
 public class Practice {
 	public static void main(String[] args) {
-			System.out.println("あなたの運勢を占います");
-			System.out.println("1〜5の中で好きな数字を入力してください");
-			int fortune = new java.util.Scanner(System.in).nextInt();
-			switch(fortune) {
-			case 1:
-				System.out.println("大吉");
-				break;
-			case 2:
-				System.out.println("中吉");
-				break;
-			case 3:
-				System.out.println("小吉");
-				break;
-			case 4:
-				System.out.println("吉");
-				break;
+			for(int i = 1; i <= 10; i++) {
+				if(i % 2 == 0) {
+					System.out.println(i);
+				}
 			}
-			
 	}
 }
