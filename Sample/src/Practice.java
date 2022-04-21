52e4a56eddcc5d0c4f6ebab825d22a2e8b1c686b
kauzki-77 <k.sora.7861@gmail.com>
2022-04-21 17:40:21 +0900

    1〜10までの数字のうち偶数のみを昇順で表示させる

--- Sample/src/Practice.java
+++ Sample/src/Practice.java
@@ -1,15 +1,10 @@
 
 public class Practice {
 	public static void main(String[] args) {
-		int sum = 0;
-		int count = 0;
 		for(int i = 1; i <= 10; i++) {
 			if(i % 2 == 0) {
-				sum += i;
-				count++;
+				System.out.println(i);
 			}
 		}
-		System.out.println(sum);
-		System.out.println(count);
 	}
 }
