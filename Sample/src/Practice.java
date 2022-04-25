3bf719131f60d0c06f19dc127c5d4a216444c081
kauzki-77 <k.sora.7861@gmail.com>
2022-04-26 00:10:48 +0900

    [add]

--- Sample/src/Practice.java
+++ Sample/src/Practice.java
@@ -4,12 +4,17 @@ public class Practice {
 		int[] score = {20, 30, 40, 50, 80};
 		int sum = 0;
 		double ave = 0;
+		int count = 0;
 		for(int i = 0; i < score.length; i++) {
 			sum = sum + score[i];
+			if(score[i] < 35) {
+				count++;
+			}
 		}
 		ave = sum / score.length;
 		System.out.println(sum);
 		System.out.println(ave);
+		System.out.println(count);
 		
 	}
 }
