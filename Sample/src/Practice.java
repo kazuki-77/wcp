c25207567df2408ca7c1f0749bbde430eef783fd
kauzki-77 <k.sora.7861@gmail.com>
2022-04-27 00:02:57 +0900

    [add]配列

--- Sample/src/Practice.java
+++ Sample/src/Practice.java
@@ -1,20 +1,13 @@
 
 public class Practice {
 	public static void main(String[] args) {
-		int[] score = {20, 30, 40, 50, 80};
-		int sum = 0;
-		double ave = 0;
-		int count = 0;
-		for(int i = 0; i < score.length; i++) {
-			sum = sum + score[i];
-			if(score[i] < 35) {
-				count++;
+		String num = "";
+		for(int i = 1; i < 10; i++) {
+			num = "";
+			for(int j = 1; j < 10; j++) {
+				num = num + j * i + " ";
 			}
+			System.out.println(num);
 		}
-		ave = sum / score.length;
-		System.out.println(sum);
-		System.out.println(ave);
-		System.out.println(count);
-		
 	}
 }
