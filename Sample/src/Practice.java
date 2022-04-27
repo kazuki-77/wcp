c48d9e853c458fb7989f68cc435b1156eb6ebcfd
kauzki-77 <k.sora.7861@gmail.com>
2022-04-27 21:57:03 +0900

    for文での九九

--- Sample/src/Practice.java
+++ Sample/src/Practice.java
@@ -2,10 +2,10 @@
 public class Practice {
 	public static void main(String[] args) {
 		String num = "";
-		for(int i = 1; i < 10; i++) {
+		for(int i = 1; i < 10; i ++) {
 			num = "";
-			for(int j = 1; j < 10; j++) {
-				num = num + j * i + " ";
+			for( int a = 1; a < 10; a++) {
+				num = num + a * i + " ";
 			}
 			System.out.println(num);
 		}
